56c4057c33b923a3a6aed123813ff3e8d501aac8
NadirJalaludeen <159204945+NadirJalaludeen@users.noreply.github.com>
2024-12-08 13:20:09 +0100

    Add part 1

--- 2024/day8.py
+++ 2024/day8.py
@@ -1,6 +1,6 @@
 import numpy as np
 
-file = open("d8.txt", "r")
+file = open("day8.txt", "r")
 
 inp = []
 for line in file:
@@ -10,6 +10,8 @@ for line in file:
         inp.append(list(line))
 
 inp = np.array(inp)
+
+#Part 1
 
 antinodes = np.zeros(np.shape(inp))
 
@@ -22,6 +24,54 @@ for f in np.unique(inp):
             for y in range(x + 1, len(posI)):
                 dI = abs(posI[x] - posI[y])
                 dJ = abs(posJ[x] - posJ[y])
+                
+                if posI[x] < posI[y]:
+                    if posJ[x] < posJ[y]:
+                        if 0 <= posI[x] - dI and 0 <= posJ[x] - dJ:
+                            antinodes[posI[x] - dI, posJ[x] - dJ] += 1
+
+                        if posI[y] + dI < len(inp) and posJ[y] + dJ < len(inp[0]):
+                            antinodes[posI[y] + dI, posJ[y] + dJ] += 1
+                            
+                    else:
+                        if 0 <= posI[x] - dI and posJ[x] + dJ < len(inp[0]):
+                            antinodes[posI[x] - dI, posJ[x] + dJ] += 1
+
+                        if posI[y] + dI < len(inp) and 0 <= posJ[y] - dJ:
+                            antinodes[posI[y] + dI, posJ[y] - dJ] += 1
+                            
+                else:
+                    if posJ[x] < posJ[y]:
+                        if posI[x] + dI < len(inp) and 0 <= posJ[x] - dJ:
+                            antinodes[posI[x] + dI, posJ[x] - dJ] += 1
+
+                        if 0 <= posI[y] - dI and posJ[y] + dJ < len(inp[0]):
+                            antinodes[posI[y] - dI, posJ[y] + dJ] += 1
+                            
+                    else:
+                        if 0 <= posI[y] - dI and 0 <= posJ[y] - dJ:
+                            antinodes[posI[y] - dI, posJ[y] - dJ] += 1
+
+                        if posI[x] + dI < len(inp) and posJ[x] + dJ < len(inp[0]):
+                            antinodes[posI[x] + dI, posJ[x] + dJ] += 1
+                            
+print(np.sum(antinodes != 0))
+
+
+#Part 2
+
+antinodes = np.zeros(np.shape(inp))
+
+for f in np.unique(inp):
+    if f == ".":
+        continue
+    else:
+        posI, posJ = np.where(inp == f)
+        for x in range(len(posI)):
+            for y in range(x + 1, len(posI)):
+                dI = abs(posI[x] - posI[y])
+                dJ = abs(posJ[x] - posJ[y])
+                
                 if posI[x] < posI[y]:
                     if posJ[x] < posJ[y]:
                         o = 0
@@ -44,6 +94,7 @@ for f in np.unique(inp):
                         while posI[y] + p * dI < len(inp) and 0 <= posJ[y] - p * dJ:
                             antinodes[posI[y] + p * dI, posJ[y] - p * dJ] += 1
                             p = p + 1
+                            
                 else:
                     if posJ[x] < posJ[y]:
                         o = 0
@@ -55,6 +106,7 @@ for f in np.unique(inp):
                         while 0 <= posI[y] - p * dI and posJ[y] + p * dJ < len(inp[0]):
                             antinodes[posI[y] - p * dI, posJ[y] + p * dJ] += 1
                             p = p + 1
+                            
                     else:
                         o = 0
                         while 0 <= posI[y] - o * dI and 0 <= posJ[y] - o * dJ:
